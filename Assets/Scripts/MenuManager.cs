befae9d9913242594f5eae7ba2a691b99ba80e93
DBGA\dbga_70 <teomani17@gmail.com>
2018-02-06 17:50:22 +0100

    Aggunto Fader per caricamento scena e script per caricare scena e chiudere il gioco

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	[Tooltip("Imposta qui la scena che vuoi caricare")]
+	public int sceneToLoad;
+
+	public void SceneByIndex(){
+		SceneManager.LoadScene (sceneToLoad);
+	}
+
+	public void LoadSceneAfter(float timeToWait ){
+		Invoke ("SceneByIndex", timeToWait);
+	}
+
+	public void CloseTheGame(){
+		Application.Quit ();
+	}
+}
