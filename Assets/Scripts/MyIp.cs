1ced13e7887f7a28a6541ee33eb2ff2ccc4e20e3
Andrea Tonnini <at.rsm@hotmail.it>
2018-02-06 16:41:22 +0100

    Commit
    [Aggiunto script da aggiungere al NetManager che prende l'indirizzo IP della macchina, inizio script classe player e weapon]

---
+++ Assets/Scripts/MyIp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class MyIp : MonoBehaviour {
+    [Header("Per ottenere IP entrare in playmode")]
+    public string myIP;
+	// Use this for initialization
+	void Start () {
+        myIP = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myIP = NetworkManager.singleton.networkAddress;
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //Stat
+    public int Health;
+    public int DamageOutput;
+    public int Defence;
+    public int PlayerSpeed;
+
+    //Combat State
+    private bool isAttacking;
+    private bool isDefending;
+
+    private bool isAlive;
+    private bool isArmed;
+
+    //Network State
+    public bool isReady;
+
+	// Use this for initialization
+	void Start () {
+        //Initialize State
+        isAttacking = false;
+        isArmed = false;
+        isAlive = true;
+        isDefending = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Health=0; GameOver;
+        if(Health<=0)
+        {
+            isAlive = false;
+        }
+        playerState();
+		if(Input.GetMouseButtonDown(0))//Tast Destro
+        {
+            //Debug.Log("Attacco");
+        }
+        if (Input.GetMouseButtonDown(1))//Tasto Sinistro
+        {
+          //  Debug.Log("Difeso");
+        }
+    }
+
+    private void playerState()
+    {
+        if (!isAlive)//se il player non ha vita, Muore;
+        {
+            Destroy(this.gameObject);
+        }
+          
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if (other.tag == "Weapon")
+        {
+            DamageOutput = other.GetComponent<Weapon>().DamageOutput;
+            Defence = other.GetComponent<Weapon>().Defence;
+            PlayerSpeed = other.GetComponent<Weapon>().PlayerSpeed;
+            Destroy(other.gameObject);
+        }
+    }
+}
