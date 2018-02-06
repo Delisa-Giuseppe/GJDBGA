fdea09b9c28a0f12537a08d08dbfa60f68881d71
Andrea Tonnini <at.rsm@hotmail.it>
2018-02-06 17:20:18 +0100

    MyipCanvas

--- Assets/Scripts/MyIp.cs
+++ Assets/Scripts/MyIp.cs
@@ -4,21 +4,34 @@ using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using UnityEngine.UI;
 
-
-public class MyIp : MonoBehaviour {
+public class MyIp : NetworkBehaviour {
     [Header("Per ottenere IP entrare in playmode")]
     public string myIP;
+
+   public Text IPText;
 	// Use this for initialization
 	void Start () {
+      //  IPText = transform.Find("Text").GetComponent<Text>();
         myIP = "";
-	}
+        IPText.text = myIP;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        myIP = LocalIPAddress();
 
+
+        myIP = LocalIPAddress();
+        if (NetworkManager.singleton.networkAddress == "localhost")
+        {
+            IPText.gameObject.SetActive(true);
+            IPText.text ="You are the host, this is your IP "+ System.Environment.NewLine + LocalIPAddress();          
+        }
     }
+    
     public string LocalIPAddress()
     {
         IPHostEntry host;
