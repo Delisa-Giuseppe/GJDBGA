d40d456ba425c0d03efbbacefada720b4f94d986
Fabio Pellicanò <fabiopellicano.gp@gmail.com>
2018-02-07 19:22:21 +0100

    Updated "Create a Lobby" option

--- Assets/Scripts/ConnectionManager.cs
+++ Assets/Scripts/ConnectionManager.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine.Networking;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
 public class ConnectionManager : NetworkBehaviour
 {
+    public Canvas firstCanvas;
+    public Canvas btnCanvas;
+
     private int playersNumber;
 
     public void StartHASHost()
@@ -20,5 +24,12 @@ public class ConnectionManager : NetworkBehaviour
     public void SetPlayerOnGame (int nPlayers)
     {
         playersNumber = nPlayers;
+        StartHASHost();
+    }
+
+    public void MoveToNPlayerCanvas()
+    {
+        firstCanvas.gameObject.SetActive(false);
+        btnCanvas.gameObject.SetActive(true);
     }
 }
